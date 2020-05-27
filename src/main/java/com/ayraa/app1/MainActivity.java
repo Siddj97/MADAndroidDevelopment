0b2fc7eb79f954de3e95a7edc755da446b55065a
Sidharth Bhardwaj <40594304+Siddj97@users.noreply.github.com>
2020-05-27 19:10:25 -0400

    my commit
    commit

--- src/main/java/com/ayraa/app1/MainActivity.java
+++ src/main/java/com/ayraa/app1/MainActivity.java
@@ -39,12 +39,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void aquarius(View view) {
-        Toast.makeText(getApplicationContext(), "Aquarius", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, Details.class);
-        intent.putExtra("sign", "aquarius".toString());
-        startActivity(intent);
-    }
+    
 
     public void virgo(View view) {
         Toast.makeText(getApplicationContext(), "Virgo", Toast.LENGTH_SHORT).show();
